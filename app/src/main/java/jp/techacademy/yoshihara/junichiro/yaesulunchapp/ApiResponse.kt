c6fcd17e8dfbaa32ea989f696d02e01679f2f12d
junichiroyoshihara <bxlove88@gmail.com>
2021-12-08 22:48:10 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yoshihara/junichiro/yaesulunchapp/ApiResponse.kt
@@ -0,0 +1,33 @@
+package jp.techacademy.yoshihara.junichiro.yaesulunchapp
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+data class ApiResponse(
+    @SerializedName("results")
+    var results: Results
+): Serializable
+
+data class Results(
+    @SerializedName("shop")
+    var shop: List<Shop>
+): Serializable
+
+data class Shop(
+    @SerializedName("coupon_urls")
+    val couponUrls: CouponUrls,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("logo_image")
+    val logoImage: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("address")
+    val address: String
+): Serializable
+
+data class CouponUrls(
+    @SerializedName("pc")
+    var pc: String,
+    @SerializedName("sp")
+    var sp: String
+): Serializable
